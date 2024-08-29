0ac91da8954fbf11117dc9a6c5f071c85c2e0663
Sowparna Shome <soupornoshome@gmail.com>
2024-08-29 20:03:44 +0530

    setting the routing

--- book-your-doctor-client/src/App.jsx
+++ book-your-doctor-client/src/App.jsx
@@ -1,7 +1,57 @@
+import { useContext, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Appointment from "./pages/Appointment";
+import AboutUs from "./pages/AboutUs";
+import Register from "./pages/Register";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Context } from "./main";
+import Login from "./pages/Login";
+const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } =
+    useContext(Context);
 
-function App() {
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8567/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
 
-  return <><h1>This is Home</h1></>
-}
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
 
-export default App
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+        <ToastContainer position="top-center" />
+      </Router>
+    </>
+  );
+};
+
+export default App;
