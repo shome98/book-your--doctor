4069ef11c7f0e9d29fd53ed97baa1a4e514716f4
Sowparna Shome <soupornoshome@gmail.com>
2024-08-29 20:02:42 +0530

    home page

---
+++ book-your-doctor-client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+//import { useContext } from "react";
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
+import MessageForm from "../components/MessageForm";
+import Departments from "../components/Deparments";
+
+const Home = () => {
+  return (
+    <>
+      <Hero
+        title={
+          "Welcome to Wonder Care Medical Institute | Your Trusted Healthcare Provider"
+        }
+        imageUrl={"/hero.png"}
+      />
+      <Biography imageUrl={"/about.png"} />
+      <Departments />
+      <MessageForm />
+    </>
+  );
+};
+
+export default Home;
